0ea7058a5b8399fe701fc725d01ba4cef56970b7
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-06-16 22:01:39 +0530

    logout view added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("register/", views.UserRegisterView.as_view(), name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,25 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views.generic import FormView
 
 from .forms import UserLoginForm, UserRegisterForm
 from .mixins import UnauthenticatedUserMixin
 
+
+class LogoutView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        return redirect('core:home')
+
+
 class UserLoginView(UnauthenticatedUserMixin, FormView):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
-    success_url = reverse_lazy('accounts:home')  # Change this to your desired URL
+    success_url = reverse_lazy('core:home')  # Change this to your desired URL
 
     def form_valid(self, form):
         login(self.request, form.get_user())
@@ -18,7 +29,7 @@ class UserLoginView(UnauthenticatedUserMixin, FormView):
 class UserRegisterView(UnauthenticatedUserMixin, FormView):
     form_class = UserRegisterForm
     template_name = 'accounts/register.html'
-    success_url = reverse_lazy('accounts:home')  # Change this to your desired URL
+    success_url = reverse_lazy('core:home')  # Change this to your desired URL
 
     def form_valid(self, form):
         user = form.save()
